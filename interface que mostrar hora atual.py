9c3e847c66196d94caf12c8a073b9fb9100817a4
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-05-23 22:05:06 -0300

    Add files via upload

---
+++ interface que mostrar hora atual.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Button, Label
+from tkinter.messagebox import showinfo
+from time import strftime, localtime
+
+def clicked():
+    time = strftime('Day: %d %b %Y\nTime: %H:%M:%S%p\n', localtime())
+    showinfo(message=time)
+
+
+root = Tk()
+button = Button(root, text='Clique', command=clicked)
+button.pack()
+root.mainloop()
